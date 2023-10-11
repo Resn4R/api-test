b3cc6c69204ba4a158ccf8ece62f61b503e929d5
Resn4R <purview-joust.0x@icloud.com>
2023-10-11 18:04:44 +0100

    Initial Commit

---
+++ image download test/image_download_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_download_testApp.swift
+//  image download test
+//
+//  Created by Vito Borghi on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct image_download_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
